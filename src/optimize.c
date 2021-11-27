a16b640d8246ab59bccaeee7be549b87b79cc9e3
Eric Wheeler <xnec2c-git@z.ewheeler.org>
2021-11-27 14:19:54 -0800

    Updated optimizer CSV output to always generate all CSV fields
    It used to only generate CSV columns if the plot or option was selected,
    now it generates all fields each time the CSV is written.

--- src/optimize.c
+++ src/optimize.c
@@ -28,6 +28,9 @@ _Write_Optimizer_Data( void )
   char csv_file[FILENAME_LEN];
   size_t s = sizeof( csv_file );
 
+  int pol, mgidx;
+  double max_gain;
+
   /* Create a file name for the Optimizer csv file */
   Strlcpy( csv_file, rc_config.input_file, s );
   Strlcat( csv_file, ".csv", s );
@@ -52,14 +55,14 @@ _Write_Optimizer_Data( void )
   } // if( !Open_File(&fp, csv_file, "w") )
 
   /* Print data column titles */
-  fprintf( fp, "%12s,", "Freq-MHz" );
-  if( isFlagSet(PLOT_ZREAL_ZIMAG) ) fprintf( fp, "%12s,%12s,", "Z-real", "Z-imag" );
-  if( isFlagSet(PLOT_ZMAG_ZPHASE) ) fprintf( fp, "%12s,%12s,", "Z-magn", "Z-phase" );
-  if( isFlagSet(PLOT_VSWR) )        fprintf( fp, "%12s,", "VSWR" );
-  if( isFlagSet(PLOT_GMAX) )        fprintf( fp, "%12s,%12s,", "Gain-max", "F/B Ratio" );
-  if( isFlagSet(PLOT_GAIN_DIR) )    fprintf( fp, "%12s,%12s,", "Direct-tht", "Direct-phi" );
-  if( isFlagSet(PLOT_GVIEWER) )     fprintf( fp, "%12s,", "Gain-viewer" );
-  if( isFlagSet(PLOT_NETGAIN) )     fprintf( fp, "%12s,", "Gain-net" );
+  fprintf( fp, "%s,", "Freq-MHz" );
+  fprintf( fp, "%s,%s,", "Z-real", "Z-imag" );
+  fprintf( fp, "%s,%s,", "Z-magn", "Z-phase" );
+  fprintf( fp, "%s,", "VSWR" );
+  fprintf( fp, "%s,%s,", "Gain-max", "F/B Ratio" );
+  fprintf( fp, "%s,%s,", "Direct-tht", "Direct-phi" );
+  fprintf( fp, "%s,", "Gain-viewer" );
+  fprintf( fp, "%s,", "Gain-net" );
   fprintf( fp, "\n" );
 
   /* Print frequency-dependent data corresponding
@@ -67,69 +70,50 @@ _Write_Optimizer_Data( void )
   for( int idx = 0; idx < calc_data.steps_total; idx++ )
   {
     /* Print the frequency in MHz */
-    fprintf( fp, "%12.4E,", (double)save.freq[idx] );
+    fprintf( fp, "%g,", (double)save.freq[idx] );
 
     /* Print Z-real and Z-imag of input impedance */
-    if( isFlagSet(PLOT_ZREAL_ZIMAG) )
-    {
-      fprintf( fp, "%12.4E,%12.4E,",
-          impedance_data.zreal[idx], impedance_data.zimag[idx] );
-    }
+	fprintf( fp, "%g,%g,",
+		impedance_data.zreal[idx], impedance_data.zimag[idx] );
 
     /* Plot Z-magnitude and Z-phase */
-    if( isFlagSet(PLOT_ZMAG_ZPHASE) )
-    {
-      fprintf( fp, "%12.4E,%12.4E,",
-          impedance_data.zmagn[idx], impedance_data.zphase[idx] );
-    }
+	fprintf( fp, "%g,%g,",
+		impedance_data.zmagn[idx], impedance_data.zphase[idx] );
 
     /* Print VSWR */
-    if( isFlagSet(PLOT_VSWR) )
-    {
-      double zrpro2 = impedance_data.zreal[idx] + calc_data.zo;
-      zrpro2 *= zrpro2;
-      double zrmro2 = impedance_data.zreal[idx] - calc_data.zo;
-      zrmro2 *= zrmro2;
-      double zimag2 = impedance_data.zimag[idx] * impedance_data.zimag[idx];
-      double gamma = sqrt( (zrmro2 + zimag2) / (zrpro2 + zimag2) );
-      double vswr = (1 + gamma) / (1 - gamma);
-      fprintf( fp, "%12.4E,", vswr );
-    }
+	double zrpro2 = impedance_data.zreal[idx] + calc_data.zo;
+	zrpro2 *= zrpro2;
+	double zrmro2 = impedance_data.zreal[idx] - calc_data.zo;
+	zrmro2 *= zrmro2;
+	double zimag2 = impedance_data.zimag[idx] * impedance_data.zimag[idx];
+	double gamma = sqrt( (zrmro2 + zimag2) / (zrpro2 + zimag2) );
+	double vswr = (1 + gamma) / (1 - gamma);
+	fprintf( fp, "%g,", vswr );
 
     /* Print Max gain for given polarization type and direction if enabled */
-    if( isFlagSet(PLOT_GMAX) && isFlagSet(ENABLE_RDPAT) )
-    {
-      int pol = calc_data.pol_type;
-      int mgidx = rad_pattern[idx].max_gain_idx[pol];
-      double max_gain = rad_pattern[idx].gtot[mgidx] + Polarization_Factor(pol, idx, mgidx);
-      fprintf( fp, "%12.4E,%12.4E,", max_gain, rad_pattern[idx].fbratio );
+	pol = calc_data.pol_type;
+	mgidx = rad_pattern[idx].max_gain_idx[pol];
+	max_gain = rad_pattern[idx].gtot[mgidx] + Polarization_Factor(pol, idx, mgidx);
+	fprintf( fp, "%g,%g,", max_gain, rad_pattern[idx].fbratio );
 
-      if( isFlagSet(PLOT_GAIN_DIR) )
-        fprintf( fp, "%12.4E,%12.4E,",
-            90.0 - rad_pattern[idx].max_gain_tht[pol],
-            rad_pattern[idx].max_gain_phi[pol] );
-    }
+	fprintf( fp, "%g,%g,",
+		90.0 - rad_pattern[idx].max_gain_tht[pol],
+		rad_pattern[idx].max_gain_phi[pol] );
 
     /* Print gain in viewer's direction */
-    if( isFlagSet(PLOT_GVIEWER) )
-    {
-      double viewer_gain = Viewer_Gain( structure_proj_params, idx );
-      fprintf( fp, "%12.4E,", viewer_gain );
-    }
+	double viewer_gain = Viewer_Gain( structure_proj_params, idx );
+	fprintf( fp, "%g,", viewer_gain );
 
     /* Print Net gain in max gain case */
-    if( isFlagSet(PLOT_NETGAIN) )
-    {
-      int pol = calc_data.pol_type;
-      int mgidx = rad_pattern[idx].max_gain_idx[pol];
-      double max_gain = rad_pattern[idx].gtot[mgidx] + Polarization_Factor(pol, idx, mgidx);
-      double Zr = impedance_data.zreal[idx];
-      double Zi = impedance_data.zimag[idx];
-      double Zo = calc_data.zo;
-      double net_gain = max_gain +
-        10.0 * log10( 4.0 * Zr * Zo / (pow(Zr + Zo, 2.0) + pow( Zi, 2.0 )) );
-      fprintf( fp, "%12.4E,", net_gain );
-    }
+	pol = calc_data.pol_type;
+	mgidx = rad_pattern[idx].max_gain_idx[pol];
+	max_gain = rad_pattern[idx].gtot[mgidx] + Polarization_Factor(pol, idx, mgidx);
+	double Zr = impedance_data.zreal[idx];
+	double Zi = impedance_data.zimag[idx];
+	double Zo = calc_data.zo;
+	double net_gain = max_gain +
+	  10.0 * log10( 4.0 * Zr * Zo / (pow(Zr + Zo, 2.0) + pow( Zi, 2.0 )) );
+	fprintf( fp, "%g,", net_gain );
 
     fprintf( fp, "\n" );
   } //for( int idx = 0; idx < calc_data.steps_total; idx++ )
